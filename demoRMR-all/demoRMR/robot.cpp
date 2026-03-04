206774ed3e75774e7746f33bcc7616227513bfdd
matej <matejmedveczky@gmail.com>
2026-03-04 16:13:27 +0100

    first commit

---
+++ demoRMR-all/demoRMR/robot.cpp
@@ -0,0 +1,172 @@
+#include "robot.h"
+#include <cmath>
+
+robot::robot(QObject *parent) : QObject(parent)
+{
+    qRegisterMetaType<LaserMeasurement>("LaserMeasurement");
+    #ifndef DISABLE_OPENCV
+    qRegisterMetaType<cv::Mat>("cv::Mat");
+#endif
+#ifndef DISABLE_SKELETON
+qRegisterMetaType<skeleton>("skeleton");
+#endif
+}
+
+void robot::initAndStartRobot(std::string ipaddress)
+{
+
+    forwardspeed=0;
+    rotationspeed=0;
+    ///setovanie veci na komunikaciu s robotom/lidarom/kamerou.. su tam adresa porty a callback.. laser ma ze sa da dat callback aj ako lambda.
+    /// lambdy su super, setria miesto a ak su rozumnej dlzky,tak aj prehladnost... ak ste o nich nic nepoculi poradte sa s vasim doktorom alebo lekarnikom...
+    robotCom.setLaserParameters([this](const std::vector<LaserData>& dat)->int{return processThisLidar(dat);},ipaddress);
+    robotCom.setRobotParameters([this](const TKobukiData& dat)->int{return processThisRobot(dat);},ipaddress);
+  #ifndef DISABLE_OPENCV
+    robotCom.setCameraParameters(std::bind(&robot::processThisCamera,this,std::placeholders::_1),"http://"+ipaddress+":8000/stream.mjpg");
+#endif
+   #ifndef DISABLE_SKELETON
+      robotCom.setSkeletonParameters(std::bind(&robot::processThisSkeleton,this,std::placeholders::_1));
+#endif
+    ///ked je vsetko nasetovane tak to tento prikaz spusti (ak nieco nieje setnute,tak to normalne nenastavi.cize ak napr nechcete kameru,vklude vsetky info o nej vymazte)
+    robotCom.robotStart();
+
+
+}
+
+void robot::setSpeedVal(double forw, double rots)
+{
+    forwardspeed=forw;
+    rotationspeed=rots;
+    useDirectCommands=0;
+}
+
+void robot::setSpeed(double forw, double rots)
+{
+    if(forw==0 && rots!=0)
+        robotCom.setRotationSpeed(rots);
+    else if(forw!=0 && rots==0)
+        robotCom.setTranslationSpeed(forw);
+    else if((forw!=0 && rots!=0))
+        robotCom.setArcSpeed(forw,forw/rots);
+    else
+        robotCom.setTranslationSpeed(0);
+    useDirectCommands=1;
+}
+
+///toto je calback na data z robota, ktory ste podhodili robotu vo funkcii initAndStartRobot
+/// vola sa vzdy ked dojdu nove data z robota. nemusite nic riesit, proste sa to stane
+int robot::processThisRobot(const TKobukiData &robotdata)
+{
+    double wheelbase = 0.23;
+    long double tick = robotCom.getTickToMeter();
+
+    static short x_old = 0;
+    static short y_old = 0;
+    static signed short angle_old = 0;
+    static short old_left_encoder = 0;
+    static short old_right_encoder = 0;
+
+    short left_distance = tick*(old_left_encoder - robotdata.EncoderLeft);
+    short right_distance = tick*(old_right_encoder - robotdata.EncoderRight);
+
+    signed short gyro_angle = robotdata.GyroAngle;
+
+    short step_dist = (wheelbase*(left_distance+right_distance)/(2*(left_distance-right_distance)));
+
+    short x_new = x_old + step_dist * sin(gyro_angle) - sin(angle_old);
+    short y_new = y_old - step_dist * cos(gyro_angle) - cos(angle_old);
+
+    x_old = x_new;
+    y_old = y_new;
+    angle_old = gyro_angle;
+
+    ///tu mozete robit s datami z robota
+    ///
+    /// max speed 400mm/s ideal 200mm/s
+
+
+
+
+///TU PISTE KOD... TOTO JE TO MIESTO KED NEVIETE KDE ZACAT,TAK JE TO NAOZAJ TU. AK AJ TAK NEVIETE, SPYTAJTE SA CVICIACEHO MA TU NATO STRING KTORY DA DO HLADANIA XXX
+
+    ///kazdy piaty krat, aby to ui moc nepreblikavalo..
+    if(datacounter%5==0)
+    {
+        cout << "Robot pos x/y";
+        cout << x_new;
+        cout << y_new;
+        ///ak nastavite hodnoty priamo do prvkov okna,ako je to na tychto zakomentovanych riadkoch tak sa moze stat ze vam program padne
+        // ui->lineEdit_2->setText(QString::number(robotdata.EncoderRight));
+        //ui->lineEdit_3->setText(QString::number(robotdata.EncoderLeft));
+        //ui->lineEdit_4->setText(QString::number(robotdata.GyroAngle));
+        /// lepsi pristup je nastavit len nejaku premennu, a poslat signal oknu na prekreslenie
+        /// okno pocuva vo svojom slote a vasu premennu nastavi tak ako chcete. prikaz emit to presne takto spravi
+        /// viac o signal slotoch tu: https://doc.qt.io/qt-5/signalsandslots.html
+        ///posielame sem nezmysli.. pohrajte sa nech sem idu zmysluplne veci
+        emit publishPosition(robotdata.EncoderLeft,y,fi);
+        ///toto neodporucam na nejake komplikovane struktury.signal slot robi kopiu dat. radsej vtedy posielajte
+        /// prazdny signal a slot bude vykreslovat strukturu (vtedy ju musite mat samozrejme ako member premmennu v mainwindow.ak u niekoho najdem globalnu premennu,tak bude cistit bludisko zubnou kefkou.. kefku dodam)
+        /// vtedy ale odporucam pouzit mutex, aby sa vam nestalo ze budete pocas vypisovania prepisovat niekde inde
+
+    }
+    ///---tu sa posielaju rychlosti do robota... vklude zakomentujte ak si chcete spravit svoje
+    if(useDirectCommands==0)
+    {
+        if(forwardspeed==0 && rotationspeed!=0)
+            robotCom.setRotationSpeed(rotationspeed);
+        else if(forwardspeed!=0 && rotationspeed==0)
+            robotCom.setTranslationSpeed(forwardspeed);
+        else if((forwardspeed!=0 && rotationspeed!=0))
+            robotCom.setArcSpeed(forwardspeed,forwardspeed/rotationspeed);
+        else
+            robotCom.setTranslationSpeed(0);
+    }
+    datacounter++;
+
+    return 0;
+
+}
+
+///toto je calback na data z lidaru, ktory ste podhodili robotu vo funkcii initAndStartRobot
+/// vola sa ked dojdu nove data z lidaru
+int robot::processThisLidar(const std::vector<LaserData>& laserData)
+{
+
+    copyOfLaserData=laserData;
+
+    //tu mozete robit s datami z lidaru.. napriklad najst prekazky, zapisat do mapy. naplanovat ako sa prekazke vyhnut.
+    // ale nic vypoctovo narocne - to iste vlakno ktore cita data z lidaru
+   // updateLaserPicture=1;
+    emit publishLidar(copyOfLaserData);
+   // update();//tento prikaz prinuti prekreslit obrazovku.. zavola sa paintEvent funkcia
+
+
+    return 0;
+
+}
+
+  #ifndef DISABLE_OPENCV
+///toto je calback na data z kamery, ktory ste podhodili robotu vo funkcii initAndStartRobot
+/// vola sa ked dojdu nove data z kamery
+int robot::processThisCamera(cv::Mat cameraData)
+{
+
+    cameraData.copyTo(frame[(actIndex+1)%3]);//kopirujem do nasej strukury
+    actIndex=(actIndex+1)%3;//aktualizujem kde je nova fotka
+
+    emit publishCamera(frame[actIndex]);
+    return 0;
+}
+#endif
+
+  #ifndef DISABLE_SKELETON
+/// vola sa ked dojdu nove data z trackera
+int robot::processThisSkeleton(skeleton skeledata)
+{
+
+    memcpy(&skeleJoints,&skeledata,sizeof(skeleton));
+
+    emit publishSkeleton(skeleJoints);
+    return 0;
+}
+#endif
